f7b18c408a72db41c83a235c643e27df149481ed
Auro Mota <aurofmota@gmail.com>
2016-10-05 00:50:37 -0300

    Using lodash

--- src/TypeData.ts
+++ src/TypeData.ts
@@ -1,3 +1,14 @@
-export default class TypeData {
+import * as _ from 'lodash';
 
+export class ArrayList<T> {
+
+    private _array: T[];
+
+    constructor(array: T[] = []) {
+        this._array = array;
+    }
+
+    get values(): T[] {
+        return this._array;
+    }
 }
